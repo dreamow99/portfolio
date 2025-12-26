cce9d663a8164fdac535dc0a9248d58009acb65e
phamduy <dreamow9@gmail.com>
2025-12-26 23:10:35 +0700

    update portfolio content

---
+++ src/config.ts
@@ -0,0 +1,124 @@
+export const siteConfig = {
+  name: "Duy Pham",
+  title: "Developer",
+  description: "Portfolio website of Duy Pham",
+  accentColor: "#f69d5d",
+  social: {
+    email: "duyphd9@gmail.com",
+    linkedin: "https://www.linkedin.com/in/duyphm/",
+    //twitter: "https://x.com/rfitzio",
+    github: "https://github.com/dreamow99",
+  },
+  aboutMe:
+    "As a game developer with more than 4 years of experience, I'm always curious to explore different aspects of software developing as well as game making",
+  skills: ["Unity", "C#", "Javascript", "Git"],
+  projects: [
+    {
+      name: "Merge Spider train",
+      descriptions: [
+        "Involved in the end-to-end development process from initial concept to launch",
+        "Developed core gameplay mechanics and implemented UI/UX systems",
+        "Integrated and managed third-party SDKs",
+        "Contributed to key decision-making for polishing and game feel optimization",
+        "Developed retention-focused features such as reward systems and engaging mini-games"
+      ],
+      link: "https://apkcombo.com/vi/merge-spider-train/merge.choo.choo.horror.train/",
+      skills: ["Unity", "C#", "Git"],
+    },
+    {
+      name: "Woody Maze - Block sort 3D",
+      descriptions: [
+        "Involved in the end-to-end development process from initial concept to launch",
+        "Developed core gameplay mechanics and implemented UI/UX systems",
+        "Elevated game design process by developing Unity Editor tool for easy level configuration"
+      ],
+      link: "https://apps.apple.com/us/app/woody-maze-block-sort-3d/id6744952014",
+      skills: ["Unity", "C#", "Git"],
+    },
+    {
+      name: "Guardian War: RPG Pixel Hero",
+      descriptions: [
+        "Involved in post-launch operations, including bug fixing and regular content updates",
+      ],
+      link: "https://apkcombo.com/vi/guardian-war-rpg-pixel-hero/com.fansipan.pixel.hero.action.rpg.adventure.games/",
+      skills: ["Unity", "C#", "Git"],
+    },
+  ],
+  experience: [
+    {
+      company: "Zitga",
+      title: "Game Developer",
+      dateRange: "June 2024 - June 2025",
+      bullets: [
+        "Developed and maintained a variety of 2D games, ranging from casual games to mid-core mobile games, using Unity Engine",
+        "Applied design patterns such as Singleton, Publisher-Subscriber (Pub-Sub), MVC, and Dependency Injection to implement robust and modular systems",
+        "Collaborated with designers and artists to create engaging animations, intuitive UI/UX, and visually appealing game worlds",
+        "Identified, debugged, and resolved complex issues across new and existing projects, ensuring smooth and bug-free gameplay experiences",
+        "Utilized Git for version control, ensuring seamless collaboration, code management, and project organization across the team"
+      ],
+    },
+    {
+      company: "Unbound Labs",
+      title: "Game Developer",
+      dateRange: "May 2021 - June 2024",
+      bullets: [
+        "Developed multiple casual 2D and 3D games for mobile platfroms (Android, iOS) using Unity Engine, with some titles surpassing 5 million downloads on the Google Play Store",
+        "Utilized Unity's NavMesh Agent to design and implement a character auto-combat mechanic",
+        "Worked with DOTween to create smooth and visually appealing animations for both character movements and user interface elements",
+        "Developed custom Unity Editor Window tools to enable game designersto easily create and configure game levels",
+        "Contributed to the full game development life-cycle, from initial concept and prototyping to deploymentand post-release updates",
+        "Integrated third-party plugins and SDKs, including Firebase for analytics and notification services, as well as ad mediation platforms to optimize monetization strategies"
+      ],
+    },
+    {
+      company: "VNG",
+      title: "Game Development Fresher",
+      dateRange: "May 2020 - Dec 2020",
+      bullets: [
+        "Collaborated in a 3-person team to complete a project developing the client-side of a simple mobile game using cocos2d-js",
+        "Implemented the A* pathfinding algorithm to enable efficient and intelligent character movement",
+        "Took part in maintaining and implementing new mini functions on the client-side for existing projects",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "VNU University of Engineering and Technology",
+      degree: "Bachelor of Computer Science",
+      dateRange: "2017 - 2021",
+      achievements: [
+        "GPA: 3.2/4.0",
+        "Developed multiple academic projects to gain a strong foundation in Object-Oriented Programming (OOP), data structures, and algorithms"
+      ],
+    },
+    {
+      school: "Hung Yen High School for the Gifted",
+      degree: "High School Diploma",
+      dateRange: "2014 - 2017",
+      achievements: [
+        "Specialized in Mathematics",
+        "Represented my province in the National High School Olympiad in Informatics",
+      ],
+    },
+  ],
+  awards: [
+    {
+      descriptiom: "Online course from CalArts",
+      title: "Introduction to Game Design",
+      dateRange: "2025",
+      link: "https://coursera.org/share/354ac3e27ccd5b358eafdb71be9fee0e",
+    },
+    {
+      descriptiom: "Issued by IIG Vietnam",
+      title: "TOEIC 895",
+      dateRange: "2020 - 2022",
+      link: "",
+    },
+    {
+      descriptiom: "Consolation Prize",
+      title: "Vietnam Olympiad in Informatics",
+      dateRange: "2015",
+      link: "",
+    },
+  ],
+};
